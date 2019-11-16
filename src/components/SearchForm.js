6fdd56024ee36488f0275c2f773f166b1214452d
AbdelIdir <homeworkoracle@protonmail.com>
2019-11-16 16:50:19 +0100

    bug fix: I added an if statement that handles the case of a search input that does not correspond to a character name. It will throw a message saying : this character does not exist

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,27 +5,52 @@ import React from "react";
 // import styled from "styled-components";
 import CharacterCard from "./CharacterCard";
 export default function SearchForm(props) {
-  return (
-    <div>
-      <div className="searched-characters">
-        {props.searchResults.name && (
-          <CharacterCard
-            name={props.searchResults.name}
-            species={props.searchResults.species}
-            status={props.searchResults.status}
-            gender={props.searchResults.gender}
-            location={props.searchResults.location.name}
-            image={props.searchResults.image}
-          />
-        )}
+  if (!props.searchResults) {
+    return (
+      <div>
+        <p>
+          This character does not exist, Please type the full name of a charater
+          again.
+        </p>
+        <form onSubmit={props.handleSubmit}>
+          <label>
+            Search for a character:
+            <input
+              type="text"
+              name="search"
+              placeholder="type the entire name"
+            />
+          </label>
+          <button>look it up !</button>
+        </form>
       </div>
-      <form onSubmit={props.handleSubmit}>
-        <label>
-          Search for a character:
-          <input type="text" name="search" placeholder="type the entire name" />
-        </label>
-        <button>look it up !</button>
-      </form>
-    </div>
-  );
+    );
+  } else
+    return (
+      <div>
+        <div className="searched-characters">
+          {props.searchResults.name && (
+            <CharacterCard
+              name={props.searchResults.name}
+              species={props.searchResults.species}
+              status={props.searchResults.status}
+              gender={props.searchResults.gender}
+              location={props.searchResults.location.name}
+              image={props.searchResults.image}
+            />
+          )}
+        </div>
+        <form onSubmit={props.handleSubmit}>
+          <label>
+            Search for a character:
+            <input
+              type="text"
+              name="search"
+              placeholder="type the entire name"
+            />
+          </label>
+          <button>look it up !</button>
+        </form>
+      </div>
+    );
 }
